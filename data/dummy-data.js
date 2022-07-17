c9ba2a970958b502bb09894b628f2d5124604ffc
akash elhance <akashelhance@gmail.com>
2022-07-17 23:50:56 +0530

    First Commit

---
+++ data/dummy-data.js
@@ -0,0 +1,33 @@
+import Category from '../models/category';
+
+
+export const CATEGORIES = [
+  new Category('c1', 'A', '#f5428d'),
+  new Category('c2', 'B', '#f54242'),
+  new Category('c3', 'C', '#f5a442'),
+  new Category('c4', 'D', '#f5d142'),
+  new Category('c5', 'E', '#368dff'),
+  new Category('c6', 'F', '#41d95d'),
+  new Category('c7', 'G', '#9eecff'),
+  new Category('c8', 'H', '#b9ffb0'),
+  new Category('c9', 'I', '#ffc7ff'),
+  new Category('c10', 'J', '#47fced'),
+
+  new Category('c11', 'K', '#f5428d'),
+  new Category('c12', 'L', '#f54242'),
+  new Category('c13', 'M', '#f5a442'),
+  new Category('c14', 'N', '#f5d142'),
+  new Category('c15', 'O', '#368dff'),
+  new Category('c16', 'P', '#41d95d'),
+  new Category('c17', 'Q', '#9eecff'),
+  new Category('c18', 'R', '#b9ffb0'),
+  new Category('c19', 'S', '#ffc7ff'),
+  new Category('c20', 'T', '#47fced'),
+
+  new Category('c21', 'U', '#f5428d'),
+  new Category('c22', 'V', '#f54242'),
+  new Category('c23', 'W', '#f5a442'),
+  new Category('c24', 'X', '#f5d142'),
+  new Category('c25', 'Y', '#368dff'),
+  new Category('c26', 'Z', '#41d95d'),
+];
